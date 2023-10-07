206619d128698f068f831b1393a597974c55e07c
ruffenach timothée <timruff@gmx.com>
2023-10-07 14:13:58 +0200

    add files shellcode windows
    add files shellcode windows

---
+++ shell-code-windows-partie-01/conv.py
@@ -0,0 +1,8 @@
+# convert string to reversed hex
+import sys
+
+input = sys.argv[1]
+chunks = [input[i:i+4] for i in range(0, len(input), 4)]
+for chunk in chunks[::-1]:
+    print (chunk[::-1].encode("utf-8").hex())
+
---
+++ shell-code-windows-partie-01/run.c
@@ -0,0 +1,14 @@
+/*
+run.c - a small skeleton program to run shellcode
+*/
+// bytecode here
+char code[] = "";
+
+int main(int argc, char **argv) {
+  int (*func)();             // function pointer
+  func = (int (*)()) code;   // func points to our shellcode
+  (int)(*func)();            // execute a function code[]
+  // if our program returned 0 instead of 1, 
+  // so our shellcode worked
+  return 1;
+}
